a65b8b748651dc26494de75fb84f39881d3a7c36
TN <amezaikupan@gmail.com>
2024-06-02 19:40:26 +0700

    Tikering with recordPerFileOption

--- EvenCount.py
+++ EvenCount.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         .appName("StructuredNetworkWordCount")\
         .getOrCreate()
         
-    spark.sparkContext.setLogLevel("ERROR") 
+    # spark.sparkContext.setLogLevel("ERROR") 
     # Read all the csv files written atomically in a directory
     userSchema = StructType([
     StructField("type", StringType(), True),
@@ -31,43 +31,70 @@ if __name__ == "__main__":
         .schema(userSchema) \
         .csv("/home/april/taxi-data")  
         
+        
+    # Version 1: Working ver   
+    # window_duration = "1 hour"
+    # window_df = df \
+    #     .withWatermark("dropoff_datetime", window_duration) \
+    #     .groupBy(window("dropoff_datetime", window_duration))\
+    #     .agg(count("*")\
+    #     .alias("event_count"))
+    #     # .orderBy('window')\
+        
+    
+    # # query = window_df\
+    # #     .writeStream\
+    # #     .outputMode("append")\
+    # #     .option("path", output_path) \
+    # #     .option("checkpointLocation", checkpoint_path)\
+    # #     .start()
+        
+    # # query = window_df\
+    # #     .writeStream\
+    # #     .outputMode("complete")\
+    # #     .format('console')\
+    # #     .option("truncate", "false") \
+    # #     .trigger(processingTime="10 minutes")\
+    # #     .start()
+
+    # write_query = window_df.select(
+    #     'window.start',
+    #     'window.end',
+    #     'event_count'
+    # ).writeStream.format('csv')\
+    #     .option('path', output_path)\
+    #     .option('checkpointLocation', checkpoint_path)\
+    #     .trigger(availableNow=True) \
+    #     .start()
+        
+    # # query.awaitTermination(timeout=600)
+    # write_query.awaitTermination(timeout=60)
+    # ------
+    
+    # Version 2: 
+    output_path = f"/home/april/BigDataLab03/EventCountOutput/output-data"
+    checkpoint_path = f"/home/april/BigDataLab03/EventCountOutput/checkpoint"
+
     window_duration = "1 hour"
-    window_df = df \
+    query = df \
         .withWatermark("dropoff_datetime", window_duration) \
         .groupBy(window("dropoff_datetime", window_duration))\
         .agg(count("*")\
-        .alias("event_count"))
-        # .orderBy('window')\
-        
+        .alias("event_count"))\
+        .select(
+            'window.start',
+            'window.end',
+            'event_count'
+        ).writeStream.format('csv')\
+            .option('path', output_path)\
+            .option('checkpointLocation', checkpoint_path)\
+            .option("maxRecordsPerFile", 1) \
+            .option("minRecordsPerFile", 1) \
+            .option("batchsize", 1)\
+            .option("outputFileName", "output_file")\
+            .trigger(processingTime='10 seconds')\
+            .start()
+            
     
-    output_path = f"/home/april/BigDataLab03/EventCountOutput/output-data"
-    checkpoint_path = f"/home/april/BigDataLab03/EventCountOutput/checkpoint"
-    # query = window_df\
-    #     .writeStream\
-    #     .outputMode("append")\
-    #     .option("path", output_path) \
-    #     .option("checkpointLocation", checkpoint_path)\
-    #     .start()
-        
-    # query = window_df\
-    #     .writeStream\
-    #     .outputMode("complete")\
-    #     .format('console')\
-    #     .option("truncate", "false") \
-    #     .trigger(processingTime="10 minutes")\
-    #     .start()
-
-    write_query = window_df.select(
-        'window.start',
-        'window.end',
-        'event_count'
-    ).writeStream.format('csv')\
-        .option('path', output_path)\
-        .option('checkpointLocation', checkpoint_path)\
-        .trigger(availableNow=True) \
-        .start()
-        
-    # query.awaitTermination(timeout=600)
-    write_query.awaitTermination(timeout=60)
-    
+    query.awaitTermination(timeout=600)
     
